d19407a7ad409ebcdd7ba0709d74eb938d8668ad
sulochan barakoti <sulochanbarakoti@gmail.com>
2024-03-25 11:53:22 +0200

    update in form

--- frontend/src/Pages/Admin/AddProduct.js
+++ frontend/src/Pages/Admin/AddProduct.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
+import { Col, Container, Form, Row, Dropdown, Button } from "react-bootstrap";
 
 const AddProduct = () => {
   return (
@@ -19,22 +19,45 @@ const AddProduct = () => {
             </Form.Group>
             <Form.Group>
               <Col>
-                {" "}
-                <Col>
-                  {" "}
-                  <Form.Label>
-                    <div className="fw-bold">Price:</div>
-                  </Form.Label>
-                  <Form.Control type="text" placeholder="Enter price" />
-                </Col>
-                <Col>
-                  {" "}
-                  <Form.Label>
-                    <div className="fw-bold">New Price:</div>
-                  </Form.Label>
-                  <Form.Control type="text" placeholder="Enter new price" />
-                </Col>
+                <Form.Label>
+                  <div className="fw-bold">Price:</div>
+                </Form.Label>
+                <Form.Control type="text" placeholder="Enter price" />
               </Col>
+              <Col>
+                <Form.Label>
+                  <div className="fw-bold">New Price:</div>
+                </Form.Label>
+                <Form.Control type="text" placeholder="Enter new price" />
+              </Col>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label className="fw-bold">Select Category:</Form.Label>
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Dropdown Button
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Another action
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Something else
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label className="fw-bold">Product Image:</Form.Label>
+              <Form.Control type="file" accept=".jpg,.png" />
+            </Form.Group>
+            <Form.Group className="py-3">
+              {" "}
+              <Button variant="outline-success" className="fw-bold">
+                Save
+              </Button>
             </Form.Group>
           </Form>
         </Row>
